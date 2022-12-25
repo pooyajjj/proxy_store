5817cd8fb1f7ec7cbfbb379a5ca1b439e0ba85fb
pooyajjj <king@kali.King>
2022-12-25 14:58:57 +0330

    class created

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        pass
+    def pots(self, request):
+        pass
