25b14d938a7dceb633007008658146f1bc0b5962
pooyajjj <pooya.jv@gmail.com>
2023-01-15 20:11:31 +0330

    issue fie=xed

--- product/serializers.py
+++ product/serializers.py
@@ -4,6 +4,16 @@ from rest_framework import serializers
 
 class ProductsSerializers(serializers.ModelSerializer):
 
+    name = serializers.CharField()
+    descraption = serializers.CharField()
+    is_stock = serializers.BooleanField()
+    slug = serializers.SlugField()
+    price = serializers.IntegerField()
+    img = serializers.ImageField()
+    created = serializers.DateTimeField()
+    update = serializers.DateTimeField()
+
+
     class Meta:
         model = products
         fields = '__all__'
--- product/views.py
+++ product/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import products
+from .models import products as pro #be carefull
 from .serializers import ProductsSerializers
 from rest_framework import status
 
 
 class ProductsView(APIView):
-    product = get_object_or_404(products, is_stock=True)
+    product = pro.objects.filter(is_stock = True)
 
     def get(self, request):
         serz_data = ProductsSerializers(instance=self.product, many=True)
-
         return Response(serz_data.data, status=status.HTTP_200_OK)
+
